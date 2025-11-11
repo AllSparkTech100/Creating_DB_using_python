fa1feea1dab6eb0016b635e7846e869100b89ccf
Abasiono Alexander <ikangenyinabasiono@gmail.com>
2025-11-11 23:23:53 +0100

    Implement context manager for DB queries
    Added a context manager for database connection and query execution.

--- DB_engine.py
+++ DB_engine.py
@@ -9,3 +9,17 @@ table_names = engine.table_names()
 
 # Print the table names to the shell
 print(table_names)
+
+
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute('SELECT LastName, Title FROM Employee')
+    df = pd.DataFrame(rs.fetchmany(size=3))
+    df.columns = rs.keys()
+
+# Print the length of the DataFrame df
+print(len(df))
+
+# Print the head of the DataFrame df
+print(df.head())
